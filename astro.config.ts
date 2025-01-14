d7c6d753f8fdcba2a2315b37b78f7d255db41da2
Lofty_Brambles <prantik.ts@gmail.com>
2025-01-14 09:22:54 +0000

    feat: add svelte plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig, envField } from "astro/config";
+import svelte from "@astrojs/svelte";
 
-import type { AstroUserConfig } from "astro";
+import type { AstroIntegration, AstroUserConfig } from "astro";
 
 const schema: NonNullable<AstroUserConfig["env"]>["schema"] = {
 	OGP_SITENAME: envField.string({ access: "public", context: "client" }),
 };
 
+const integrations: AstroIntegration[] = [svelte()];
+
 export default defineConfig({
+	experimental: { contentIntellisense: true, svg: { mode: "sprite" } },
 	env: { schema },
+	integrations,
 });
